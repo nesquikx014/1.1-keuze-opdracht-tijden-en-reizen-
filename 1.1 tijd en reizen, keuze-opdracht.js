41161b4fcbc9c2a7826908e0c0cc18032627a64a
nesquikx014 <n.eroglux014@gmail.com>
2024-10-10 16:23:38 +0200

    Add files via upload

---
+++ 1.1 tijd en reizen, keuze-opdracht.js
@@ -0,0 +1,11 @@
+const date = new Date(1985, 6, 12);
+console.log(date);
+const date2 = new Date(1955, 9, 23);    
+console.log(date2);
+const date3 = new Date(2015, 7,  31);    
+console.log(date3);
+const date4 = new Date(1885, 0, 4, 10, 39, 5);    
+console.log(date4);
+"console"; "integratedTerminal"
+
+
